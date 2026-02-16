02eba7796a2bffb5659dc8d7755d9d75ab7a27ff
bmukesh23 <billamukesh1234@gmail.com>
2026-02-16 12:31:41 +0530

    feat: variables

---
+++ 2_simple_values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+
+	// int
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("hello world")
+
+	// bool
+	fmt.Println(true)
+
+	// floats
+	fmt.Println(13.5)
+}
---
+++ 3_variables/variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "mukesh"
+	fmt.Println(name)
+
+	var age int = 30
+	fmt.Println(age)
+
+	// infer
+	var isMarried = true
+	fmt.Println(isMarried)
+
+	// shorthand syntax
+	name2 := "mukesh"
+	fmt.Println(name2)
+}
