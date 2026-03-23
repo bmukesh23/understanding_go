4d5fcf946481011a3f7edec42db3799eeb2ae070
Mukesh Billa <billamukesh1234@gmail.com>
2026-03-23 12:42:39 +0530

    feat: pointers in go

---
+++ 15_pointers/pointers.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// go passes arguments by value, so when we pass num to changeNum, it creates a copy of num. Therefore, when we change num inside changeNum, it does not affect the original num in main. The output will be:
+// In changeNum 5
+// After changeNum in main 1
+
+// by value
+// func changeNum(num int) {
+// 	num = 5
+// 	fmt.Println("In changeNum", num)
+// }
+
+// by reference
+func changeNum(num *int) {
+	*num = 5
+	fmt.Println("In changeNum", *num)
+}
+
+func main() {
+	num := 1
+	changeNum(&num)
+	fmt.Println("After changeNum in main", num)
+
+	// To see memory addresses, we can use the & operator to get the address of a variable.
+	// fmt.Println("memory address", &num)
+}
