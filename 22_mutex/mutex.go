bc967aa4de2f57bd3ea243a3dd113fa2cc64acf1
Mukesh Billa <billamukesh1234@gmail.com>
2026-04-03 16:31:46 +0530

    feat: mutex in go

--- 22_mutex/mutex.go
+++ 22_mutex/mutex.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+// mutex -> is also known as mutual exclusion lock.
+// it is used to prevent race conditions when multiple goroutines access shared data concurrently.
+// it is a lock that ensures that only one goroutine accesses shared data at a time.
+var counter int
+var mu sync.Mutex
+
+func increment(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	mu.Lock() // lock
+	counter++
+	mu.Unlock() // unlock
+}
+
 func main() {
 
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go increment(&wg)
+	}
+
+	wg.Wait()
+	fmt.Println(counter)
 }
