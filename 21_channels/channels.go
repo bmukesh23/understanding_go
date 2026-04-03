feb36fed4060aa5b6c681372f76fd57fcf1867e8
Mukesh Billa <billamukesh1234@gmail.com>
2026-04-03 11:55:30 +0530

    feat: channels in go

--- 21_channels/channels.go
+++ 21_channels/channels.go
@@ -2,17 +2,56 @@ package main
 
 import "fmt"
 
-// channels are a way to communicate between goroutines. They are a powerful tool for concurrent programming in Go. Channels can be used to send and receive values between goroutines, and they can also be used to synchronize the execution of goroutines.
+// channels -> these are a way to communicate between goroutines.
+
+// unbuffered channels: send and receive must happen at the same time.
+// func main() {
+// 	// define a channel of type string
+// 	ch := make(chan string)
+
+// 	go func() {
+// 		// send a message to the channel
+// 		ch <- "Hello, World!"
+// 	}()
+
+// 	// receive a message from the channel
+// 	msg := <-ch // this waits until data is available in the channel
+// 	fmt.Println(msg)
+// }
+
+// unbuffered channels, send and receive must run in different goroutines.
+// new goroutine
+//      |
+//      |---- send (ch <- "Hello World")
+// main goroutine
+//      |
+//      |---- receive  (<-ch)
+
+// buffered channels: send and receive can happen at different times.
 func main() {
-	// define a channel of type string
-	ch := make(chan string)
+	ch := make(chan int, 2) // buffer size of 2
+
+	// send messages to the channel
+	ch <- 1
+	ch <- 2
+
+	// receive messages from the channel
+	fmt.Println(<-ch) // prints 1
+	fmt.Println(<-ch) // prints 2
+}
+
+// closing channels: a channel can be closed when no more values will be sent.
+// example:
+/*
+	ch := make(chan int)
 
 	go func() {
-		// send a message to the channel
-		ch <- "Hello, World!"
+     	ch <- 1
+     	ch <- 2
+     	close(ch)
 	}()
 
-	// receive a message from the channel
-	msg := <-ch // this waits until data is available in the channel
-	fmt.Println(msg)
-}
+ 	for val := range ch {
+    	fmt.Println(val)
+	}
+*/
